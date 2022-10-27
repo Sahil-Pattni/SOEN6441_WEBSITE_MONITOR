2b15d8c6540f7727854c8763f17c909738209a9b
Sahil Pattni <sahilpattni97@gmail.com>
2022-10-27 16:16:00 -0400

    Refactor + Documentation

--- src/monitor.py
+++ src/monitor.py
@@ -10,7 +10,7 @@ env = os.environ
 
 class WebpageMonitor:
     """
-    TODO:
+    Monitors the webpage for changes by tracking the hash of the webpage.
     """
     def __init__(self, recipients:list, url='http://users.encs.concordia.ca/~cc/soen6441/', verbose=False) -> None:
         self.url = url
@@ -38,25 +38,34 @@ class WebpageMonitor:
         # Create hash of response webpage hexdigest
         return hashlib.sha256(response.text.encode('utf-8')).hexdigest()
 
+    def __set_hash(self, new_hash):
+        """
+        Sets the hash of the webpage in the environment variables.
+        """
+        self.__log(f'Setting hash to {new_hash}')
+        os.system(f'heroku config:set -a soen6441-bot LAST_HASH={new_hash}')
+        self.last_hash = new_hash
+
 
     def monitor_webpage(self):
         """
-        TODO:
+        Monitors the hash of the webpage for changes.
         """
-
+        new_hash = self.__get_hash()
         try:
             self.last_hash = env['LAST_HASH']
             # Get updated hash of webpage
-            new_hash = self.__get_hash()
             # If new hash is different, print message and update last hash
             if new_hash != self.last_hash:
                 self.__log(f'Webpage change detected, sending email...')
                 self.email_client.send_email(f'Webpage change detected at {self.url} at {time.ctime()}')
+                self.__set_hash(new_hash)
+                
             else:
                 self.__log(f'No change detected at {time.ctime()}')
                 
-        except KeyError as e:
+        except KeyError as _:
+            # If last hash is not set, set it to the current hash
             self.__log('No website hash found in the environment variables. Setting hash...')
-            self.last_hash = new_hash
-            os.system(f'heroku config:set -a soen6441-bot LAST_HASH={self.last_hash}')
+            self.__set_hash(new_hash)
             return
