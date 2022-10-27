283579bc3eb4c9165e219e7f151333cd85310336
Sahil Pattni <sahilpattni97@gmail.com>
2022-10-27 15:39:28 -0400

    Created main app to run monitor

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from email_client import EmailClient
 from monitor import WebpageMonitor
-
+# %%
 if __name__ == '__main__':
-    # Initialize email client
+    # Set recipients
     recipients = ['sahilpattni97+SOENBOT@gmail.com']
-    email_client = EmailClient(recipients)
-    email_client.send_email('Hello world!')
+    # Create monitor object
+    monitor = WebpageMonitor(recipients, verbose=True)
+    # Run monitor
+    monitor.monitor_webpage()
+
+# %%
